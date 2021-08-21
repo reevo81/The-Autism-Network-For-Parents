d7ceb8b77c771ba493f88da08408ed65739fa12e
steven orourke <stevenorourke@live.co.uk>
2021-08-21 13:56:53 +0100

    Forum DB update

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,60 @@
 const postList = document.querySelector('#postTable');
+const postForm = document.querySelector('#postCreate');
+
 // create elements and render to DOM
 function renderPosts(doc) {
-    let li = document.createElement('li');
-    let name = document.createElement('span');
-    let desc = document.createElement('span');
 
-    li.setAttribute('data-id', doc.id);
-    name.textContent = doc.data().postName;
-    desc.textContent = doc.data().postDesc;
-    
-    li.appendChild(name);
-    li.appendChild(desc);
-    
-    postList.appendChild(li);
+    const table = document.getElementById("testBody");
+      let row = table.insertRow();
+      let name = row.insertCell(0);
+      name.innerHTML = doc.data().postName;
+      let desc = row.insertCell(1);
+      desc.innerHTML = doc.data().postDesc;
+      let author = row.insertCell(2);
+      author.innerHTML = doc.data().userName;
 }
 
+/*
 db.collection('posts').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderPosts(doc);
-    })  
+		console.log(doc.id, " => ", doc.data());
+			var replies = db.collectionGroup('replies');
+				replies.get().then((querySnapshot) => {
+				querySnapshot.forEach((rep) => {
+				console.log(rep.id, ' => ', rep.data());
+			});
+		});
+    });  
+});
+*/
+
+// get updates in real-time
+db.collection('posts').onSnapshot(snapshot => {
+	let changes = snapshot.docChanges();
+	changes.forEach(change => {
+		var replies = db.collectionGroup('replies');
+		replies.get().then((querySnapshot) => {
+		querySnapshot.forEach((rep) => {
+		console.log(rep.id, ' => ', rep.data());
+		})
+			if(change.type == 'added'){
+				renderPosts(change.doc);
+				console.log(change.doc.id, " => ", change.doc.data());
+			}
+		})
+	})
+})
+
+postForm.addEventListener('submit', (post) => {
+	post.preventDefault();
+	db.collection("posts").add({
+		postName: postForm.postTitle.value,
+		postDesc: postForm.postContent.value,
+		profilePic: "Unknown",
+		userName: "Unknown"
+	})
+	postForm.postTitle.value = '';
+	postForm.postContent.value = '';
+	$("#collapseNewPost").collapse({ 'toggle': false }).collapse('hide');
 })
