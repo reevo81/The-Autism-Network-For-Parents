406ec395deb2f6d336fa2344e1871c3062997ece
steven orourke <stevenorourke@live.co.uk>
2021-09-20 13:17:48 +0100

    Final Commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,51 +1,64 @@
 const postList = document.querySelector('#postTable');
 const postForm = document.querySelector('#postCreate');
 
-// create elements and render to DOM
+// create elements and render to DOM post table element
 function renderPosts(doc) {
 
-    const table = document.getElementById("testBody");
+    const table = document.getElementById("renderBody");
       let row = table.insertRow();
+	  row.id = doc.id;
+	  row.name = "";
+	  console.log('Doc ID is... ' + row.id)
       let name = row.insertCell(0);
       name.innerHTML = doc.data().postName;
       let desc = row.insertCell(1);
       desc.innerHTML = doc.data().postDesc;
       let author = row.insertCell(2);
       author.innerHTML = doc.data().userName;
+// Test to put replies array into 4th column and match to each post 
+//      let reply = row.insertCell(3);
+//      reply.innerHTML = doc.data().replies;
+// Function to get replies into new rows beneath each post. Not currently working!! 	
+//		var replies = db.collection("posts/" + row.id + "/replies");
+//		replies.get().then((querySnapshot) => {
+//				querySnapshot.forEach((doc) => {
+//				let row = table.insertRow();
+//				let name = row.insertCell(0);
+//				name.innerHTML = doc.data().reply;
+//				console.log(doc.id, ' => ', doc.data());
+//				console.log(doc.data().reply);
+//			});
+//		});
+
 }
 
-/*
-db.collection('posts').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        renderPosts(doc);
-		console.log(doc.id, " => ", doc.data());
-			var replies = db.collectionGroup('replies');
-				replies.get().then((querySnapshot) => {
-				querySnapshot.forEach((rep) => {
-				console.log(rep.id, ' => ', rep.data());
-			});
-		});
-    });  
-});
-*/
+// This function does not work. Possibly because no tr tag has been created in the HTML table!!!  
+//$(document).ready(function(){
+//    $("tableSelector").delegate("td", "click", function(){
+//        alert("Click!");
+//    });
+//});
 
 // get updates in real-time
 db.collection('posts').onSnapshot(snapshot => {
 	let changes = snapshot.docChanges();
 	changes.forEach(change => {
-		var replies = db.collectionGroup('replies');
-		replies.get().then((querySnapshot) => {
-		querySnapshot.forEach((rep) => {
-		console.log(rep.id, ' => ', rep.data());
-		})
-			if(change.type == 'added'){
-				renderPosts(change.doc);
-				console.log(change.doc.id, " => ", change.doc.data());
-			}
-		})
+		if(change.type == 'added'){
+			renderPosts(change.doc);
+			console.log(change.doc.id, " => ", change.doc.data());
+		
+		}
+//		var replies = db.collection("posts/" + change.doc.id + "/replies");
+//		replies.get().then((querySnapshot) => {
+//				querySnapshot.forEach((doc) => {
+//				console.log(doc.id, ' => ', doc.data());
+//				console.log(doc.data().reply);
+//			});
+//		});
 	})
 })
-
+		
+// Add Event Listener to create post button
 postForm.addEventListener('submit', (post) => {
 	post.preventDefault();
 	db.collection("posts").add({
